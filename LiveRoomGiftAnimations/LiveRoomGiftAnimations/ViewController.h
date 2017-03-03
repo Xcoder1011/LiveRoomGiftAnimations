62b3178ba0bae90d7ce71a6e2312a4a77926ce36
KUN <wushangkun@youqingxu.com>
2017-03-03 18:42:30 +0800

    initial commit

---
+++ LiveRoomGiftAnimations/LiveRoomGiftAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveRoomGiftAnimations
+//
+//  Created by KUN on 17/3/3.
+//  Copyright © 2017年 animation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LiveRoomGiftAnimations/LiveRoomGiftAnimations/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  LiveRoomGiftAnimations
+//
+//  Created by KUN on 17/3/3.
+//  Copyright © 2017年 animation. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 1. 评论 tableview 上滚
+    // 2. 礼物汽车（轮子 滚动)
+    // 3. 送花 连击效果
+    // 4. 点赞 动画（粒子特效）
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
